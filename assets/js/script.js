cc1c32c588dba1e06290a52f8fd3050198e17e49
T <endlesstruffle80@gmail.com>
2022-09-27 22:39:53 -0700

    Changes api used. Adds ability to display todays date,weather conditions, and location. Also display s5 day forecast with date and conditions. No emojis yet.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
-const $todaysWeatherDisplay = document.querySelector('#todays-weather')
+const $todaysWeather = document.querySelector('#todays-weather')
+const $todaysConditions = document.querySelector('#todays-conditions');
+const $forecastDisplay = document.querySelector('#forecast');
 const $prevSearchDisplay = document.querySelector('#prev-search');
 const $citySearchForm = document.querySelector('#city-search');
 let prevSearches = [];
-let city;
-
 
 function init() {
 
@@ -17,22 +17,38 @@ function init() {
 
 function getCityWeather(city) {
 
-    const weatherLink = "https://api.openweathermap.org/data/2.5/forecast?q=";
-    const apiKey = "0d7724b53ab6199eface06b3174e7688";
+    const weatherLink = "https://api.weatherbit.io/v2.0/forecast/daily?city=";
+    const apiKey = "47a5f13b361942e680fd8c4ceca2bdfc";
+    let weatherData;
 
-    const requestUrl = `${weatherLink}${city}&appid=${apiKey}&cnt=6&units=imperial`
+    const requestUrl = `${weatherLink}${city}&key=${apiKey}&units=I&days=6`
 
     fetch(requestUrl)
     .then(function (response) {
+
       if (response.ok) {
 
         response.json()
 
         .then(function (data) {
 
-          console.log(data);
+            weatherData = data;
+            console.log(weatherData);
+            displayWeather(weatherData);
+            displayForecast(weatherData);
 
         });
+
+        if (!prevSearches.includes(city)) {
+
+            prevSearches.push(city);
+
+            localStorage.setItem('prevSearchArr', prevSearches);
+
+        }
+
+        refreshSearchDisplay();
+        displaySearches();
 
       } else {
 
@@ -45,6 +61,63 @@ function getCityWeather(city) {
       alert('Unable to connect to weather');
       
     });
+
+}
+
+function displayWeather(weatherData) {
+
+    const cityName = weatherData.city_name;
+    const todaysDate = moment(weatherData.data[0].valid_date).format("(MM/DD/YYYY)");
+    const todaysTemp = `Temp: ${weatherData.data[0].temp}\xB0F`;
+    const todaysWind = `Wind: ${weatherData.data[0].wind_spd} MPH`;
+    const todaysHumidity = `Humidity: ${weatherData.data[0].rh}%`;
+    const todaysUv = `UV Index: ${weatherData.data[0].uv}`;
+    const conditionsArray = [todaysTemp,todaysWind,todaysHumidity,todaysUv];
+    //let todaysSymbol = `0x1${weatherData.data[i].weather.icon}`;
+
+    $todaysWeather.textContent = `${cityName} ${todaysDate}`;
+
+    for (let i = 0; i < conditionsArray.length; i++) {
+
+        const $conditionItem = document.createElement('li');
+
+        $conditionItem.textContent = conditionsArray[i];
+
+        $todaysConditions.append($conditionItem);
+
+    }
+
+}
+
+function displayForecast(weatherData) {
+
+    for(let i = 1; i < weatherData.data.length; i++) {
+
+        const date = moment(weatherData.data[i].valid_date).format("(MM/DD/YYYY)");
+        const temp = `Temp: ${weatherData.data[i].temp}\xB0F`;
+        const wind = `Wind: ${weatherData.data[i].wind_spd} MPH`;
+        const humidity = `Humidity: ${weatherData.data[i].rh}%`;
+        const conditionsArray = [temp,wind,humidity];
+        const $forecastSection = document.createElement('section');
+        const $forecastDate = document.createElement('h4');
+        //let symbol = `0x1${weatherData.data[i].weather.icon}`;
+
+        $forecastDate.textContent = date;
+        $forecastSection.append($forecastDate)
+
+        for (let x = 0; x < conditionsArray.length; x++) {
+
+            const $conditionItem = document.createElement('p');
+
+            $conditionItem.textContent = conditionsArray[x];
+
+            $forecastSection.append($conditionItem);
+
+        }
+
+        $forecastDisplay.append($forecastSection);
+
+    }
 
 }
 
@@ -65,35 +138,30 @@ function displaySearches() {
 function refreshSearchDisplay() {
 
     while ($prevSearchDisplay.firstChild) {
+
         $prevSearchDisplay.removeChild($prevSearchDisplay.firstChild);
+
     }
 
 }
+
+
 
 function handleFormSubmit(event) {
     event.preventDefault();
 
     const $cityInput = document.querySelector('#city-input');
-
-    city = $cityInput.value.trim();
+    let city = $cityInput.value.trim();
     city = city.toLowerCase();
 
-    if(!prevSearches.includes(city) && city !== "") {
-
-        prevSearches.push(city);
-
-        localStorage.setItem('prevSearchArr', prevSearches);
-
-        getCityWeather(city);
-        refreshSearchDisplay();
-        displaySearches();
-
-    } else if (prevSearches.includes(city)) {
+    if(city !== "") {
 
         getCityWeather(city);
 
     } else if (city === "") {
+
         console.log("Please Enter a City");
+
     }
 }
 
