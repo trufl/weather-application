e54e1a3fe44e3d1798919c981b9e722b75471b35
T <endlesstruffle80@gmail.com>
2022-09-28 00:08:43 -0700

    Adds emojis to represent weather. Prevents old weather data from cluttering the screen. Prev searches still dont work.

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,6 +24,7 @@ function getCityWeather(city) {
     const requestUrl = `${weatherLink}${city}&key=${apiKey}&units=I&days=6`
 
     fetch(requestUrl)
+
     .then(function (response) {
 
       if (response.ok) {
@@ -34,6 +35,7 @@ function getCityWeather(city) {
 
             weatherData = data;
             console.log(weatherData);
+            refreshWeatherDisplay();
             displayWeather(weatherData);
             displayForecast(weatherData);
 
@@ -52,7 +54,7 @@ function getCityWeather(city) {
 
       } else {
 
-        alert('Error: ' + response.statusText);
+        alert('Please enter a valid city');
 
       }
     })
@@ -68,18 +70,18 @@ function displayWeather(weatherData) {
 
     const cityName = weatherData.city_name;
     const todaysDate = moment(weatherData.data[0].valid_date).format("(MM/DD/YYYY)");
+    const todaysSymbol = getEmoji(weatherData.data[0].weather.code);
     const todaysTemp = `Temp: ${weatherData.data[0].temp}\xB0F`;
     const todaysWind = `Wind: ${weatherData.data[0].wind_spd} MPH`;
     const todaysHumidity = `Humidity: ${weatherData.data[0].rh}%`;
     const todaysUv = `UV Index: ${weatherData.data[0].uv}`;
     const conditionsArray = [todaysTemp,todaysWind,todaysHumidity,todaysUv];
-    //let todaysSymbol = `0x1${weatherData.data[i].weather.icon}`;
 
-    $todaysWeather.textContent = `${cityName} ${todaysDate}`;
+    $todaysWeather.textContent = `${cityName} ${todaysDate} ${todaysSymbol}`;
 
     for (let i = 0; i < conditionsArray.length; i++) {
 
-        const $conditionItem = document.createElement('li');
+        const $conditionItem = document.createElement('p');
 
         $conditionItem.textContent = conditionsArray[i];
 
@@ -94,13 +96,13 @@ function displayForecast(weatherData) {
     for(let i = 1; i < weatherData.data.length; i++) {
 
         const date = moment(weatherData.data[i].valid_date).format("(MM/DD/YYYY)");
+        const symbol = getEmoji(weatherData.data[i].weather.code);
         const temp = `Temp: ${weatherData.data[i].temp}\xB0F`;
         const wind = `Wind: ${weatherData.data[i].wind_spd} MPH`;
         const humidity = `Humidity: ${weatherData.data[i].rh}%`;
-        const conditionsArray = [temp,wind,humidity];
+        const conditionsArray = [symbol,temp,wind,humidity];
         const $forecastSection = document.createElement('section');
         const $forecastDate = document.createElement('h4');
-        //let symbol = `0x1${weatherData.data[i].weather.icon}`;
 
         $forecastDate.textContent = date;
         $forecastSection.append($forecastDate)
@@ -135,6 +137,24 @@ function displaySearches() {
 
 }
 
+function refreshWeatherDisplay() {
+
+    $todaysWeather.textContent = "";
+
+    while($todaysConditions.firstChild) {
+
+        $todaysConditions.removeChild($todaysConditions.firstChild);
+
+    }
+
+    while($forecastDisplay.firstChild) {
+
+        $forecastDisplay.removeChild($forecastDisplay.firstChild);
+
+    }
+
+}
+
 function refreshSearchDisplay() {
 
     while ($prevSearchDisplay.firstChild) {
@@ -145,7 +165,60 @@ function refreshSearchDisplay() {
 
 }
 
+function getEmoji(emojiCode) {
 
+    switch(emojiCode) {
+        case 200:
+        case 201:
+        case 202:
+            return "🌦";
+        case 230:
+        case 231:
+        case 232:
+        case 233:
+            return "🌩";
+        case 300:
+        case 301:
+        case 302:
+        case 500:
+        case 501:
+        case 502:
+        case 511:
+        case 520:
+        case 521:
+        case 522:
+            return "🌧";
+        case 600:
+        case 601:
+        case 602:
+        case 610:
+        case 611:
+        case 612:
+        case 621:
+        case 622:
+        case 623:
+            return "🌨";
+        case 700:
+        case 711:
+        case 721:
+        case 731:
+        case 741:
+        case 751:
+            return "🌥";
+        case 800:
+            return "☀️";
+        case 801:
+        case 802:
+        case 803:
+            return "🌤";
+        case 804:
+            return "☁️";
+        default:
+            return "⛈";
+    }
+
+
+}
 
 function handleFormSubmit(event) {
     event.preventDefault();
@@ -157,6 +230,7 @@ function handleFormSubmit(event) {
     if(city !== "") {
 
         getCityWeather(city);
+        $cityInput.value = "";
 
     } else if (city === "") {
 
